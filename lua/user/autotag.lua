f0f5a37376182718ad2c5cde2bae9cb5f4bef57f
Tham <tham@tomosia.com>
2022-12-14 21:40:12 +0700

    Add git blame

---
+++ lua/user/autotag.lua
@@ -0,0 +1,7 @@
+local autotag_status_ok, autotag = pcall(require, "nvim-ts-autotag")
+if not autotag_status_ok then
+  return
+end
+
+autotag.setup()
+
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,6 +56,7 @@ return packer.startup(function(use)
   use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
 	use {"folke/which-key.nvim"}
+	use {"windwp/nvim-ts-autotag"}
 
 	-- Colorschemes
   use { "catppuccin/nvim", as = "catppuccin" }
