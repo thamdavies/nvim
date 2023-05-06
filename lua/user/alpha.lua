85764d4817507a7db07abded11160b889d2f3463
Tham <thamdavies@gmail.com>
2023-05-06 13:42:59 +0700

    Update

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "thamdavies@neovim"
+	return "thamdavies.com"
 end
 
 dashboard.section.footer.val = footer()
