0ab1f4f618cb8a0e40eb4a873ef81baa796f4f42
Tham <thamdavies@gmail.com>
2024-04-10 11:20:53 +0700

    updated typescript LSP

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,7 +22,7 @@ return {
   		ensure_installed = {
   			"lua-language-server", "stylua",
   			"html-lsp", "css-lsp" , "prettier",
-        "solargraph", "tsserver"
+        "solargraph", "typescript-language-server",
   		},
   	},
   },
